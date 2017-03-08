7cf4a276c8ab4bc3f59e6f88e264b5f5a7aedeea
Laura <buncelful@gmail.com>
2017-03-08 11:30:28 -0500

    Updated the commenting in the PHP file to say 2013 child and updated style sheet to change background colour to black,font to blue and navbar to yellow.

--- wp-content/themes/childtwentythirteen/functions.php
+++ wp-content/themes/childtwentythirteen/functions.php
@@ -1,9 +1,9 @@
 <?php
 function my_theme_enqueue_styles() {
 
-   $parent_style = 'twentythirteen-style'; // This is 'twentythirteen-style' for the Twenty Fifteen theme.
+   $parent_style = 'twentythirteen-style'; // This is 'twentythirteen-style' 
 
-   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css');
    wp_enqueue_style( 'child-style',
        get_stylesheet_directory_uri() . '/style.css',
        array( $parent_style ),
